c2f2b188b20f6321e2d054a73679a33023b7b537
Eleonore Vandendaele <eleonore.vandendaele@skynet.be>
2023-05-15 23:12:39 +0200

    Commentaires des codes de la mine

--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -9,12 +9,14 @@ public class Resource : MonoBehaviour
     public GameObject animationFx;
     public Transform PrefabSpawn;
     public AudioSource audio;
-
+    
+    //Appel de la fonction Tap()
     public void Tapped()
     {
         StartCoroutine(Tap());   
     }
 
+    //Cette fonction lance l'animation de poussière, l'audio des rochers, fait apparaitre les petits cailloux et étruit le plus gros caillou de départ
     IEnumerator Tap()
     {   
         yield return new WaitForSeconds(0.25f);
